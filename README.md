cb98356f653e87f420077177c9f22d5c7157f111
danschnurpfeil <aaa>
2023-09-21 13:00:42 +0200

    svg gen

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # kiv-ppr
+- Cílem práce je najít vzorec pro korelaci mezi 3D akcelerometrem a srdečním tepem. Z [https://physionet.org/content/big-ideas-glycemic-wearable/1.1.2/](https://physionet.org/content/big-ideas-glycemic-wearable/1.1.2/) si stáhněte přísloušnou část datových souborů - HR a ACC soubory. Pak napište jednoduchý generátor funkcí s následujícím prototypem:
+
+`double transform(const double acc_x, const double acc_y, const double acc_z);`
+
+- Pro výslednou časovou řadu spočítejte její korelaci se srdečním tepem. Čím menší hodnota, tím lepší korelace. Tím vlastně dostanete fitness funkci, pokud budete generovat těla transformační funkcí evolučním/genetickým algoritmem, jako to např. dělá genetické programovaní nebo gramatická evoluce.
+
+- Celý výpočet spusťte na OpenCL zařízení - tj. nemusíte psát parser výrazů, ale OpenCL driver to udělá za vás. Je vhodné, aby se výpočet míry korelace a transformace odehrál celý na GPU, čímž se pak vyhnete zbytečnému kopírování dat.
+Ve finále vyberte transformační funkci s co největší korelací a tu zobrazte v grafu společně se srdeční frekvencí. Graf vygenerujte jako .svg.
+
+
+poznámky
+
+- preprocess.: normalizace dat
+
+- transformační funkci -GPU gentický alg. - vektorově (CPU)
---
+++ svg_gen.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <fstream>
+
+int main() {
+    // Create an output file stream
+    std::ofstream svgFile("plot.svg");
+
+    // Check if the file opened successfully
+    if (!svgFile.is_open()) {
+        std::cerr << "Error: Unable to open the SVG file." << std::endl;
+        return 1;
+    }
+
+    // Define the width and height of the SVG canvas
+    int width = 450;
+    int height = 300;
+
+    // Generate the SVG code
+    svgFile << "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>" << std::endl;
+    svgFile << "<svg width=\"" << width << "\" height=\"" << height << "\" xmlns=\"http://www.w3.org/2000/svg\">" << std::endl;
+
+    // Create a white background rectangle
+    svgFile << "  <rect x=\"0\" y=\"0\" width=\"" << width << "\" height=\"" << height << "\" fill=\"white\" />" << std::endl;
+
+    // Create X-axis
+    svgFile << "  <line x1=\"50\" y1=\"250\" x2=\"350\" y2=\"250\" stroke=\"black\" stroke-width=\"2\" />" << std::endl;
+
+    // Create Y-axis
+    svgFile << "  <line x1=\"50\" y1=\"250\" x2=\"50\" y2=\"50\" stroke=\"black\" stroke-width=\"2\" />" << std::endl;
+
+    // X-axis label
+    svgFile << "  <text x=\"375\" y=\"275\" font-size=\"16\" fill=\"black\">X-axis</text>" << std::endl;
+
+    // Y-axis label
+    svgFile << "  <text x=\"0\" y=\"15\" font-size=\"16\" fill=\"black\" transform=\"rotate(270, 25, 25)\">Y-axis</text>" << std::endl;
+
+    // Define example data points
+    int dataPoints[] = {10, 10, 10, 180, 160, 140, 120};
+
+    // Create the polyline
+    svgFile << "  <polyline points=\"";
+    for (int i = 0; i < 7; i++) {
+        svgFile << 50 + dataPoints[i] << "," << 250 - dataPoints[i] << " ";
+    }
+    svgFile << "\" fill=\"none\" stroke=\"black\" stroke-width=\"2\" />" << std::endl;
+
+    // Title
+    svgFile << "  <text x=\"200\" y=\"30\" font-size=\"20\" fill=\"black\" text-anchor=\"middle\">SVG Polyline Plot</text>" << std::endl;
+
+    // X-axis captions
+    for (int i = 0; i < 7; i++) {
+        svgFile << "  <text x=\"" << 50 + i * 50 << "\" y=\"265\" font-size=\"12\" fill=\"black\">" << i << "</text>" << std::endl;
+    }
+
+    // Y-axis values and captions
+    for (int i = 0; i < 7; i++) {
+        svgFile << "  <text x=\"25\" y=\"" << 50 + i * 30 << "\" font-size=\"12\" fill=\"black\">" << 30 * i << "</text>" << std::endl;
+    }
+
+    // Close the SVG file
+    svgFile << "</svg>" << std::endl;
+
+    // Close the file stream
+    svgFile.close();
+
+    std::cout << "SVG plot with white background and black lines created successfully in 'plot.svg'." << std::endl;
+
+    return 0;
+}
