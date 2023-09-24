41049f3ccf93218c6f6d61463c9497a224c84692
danschnurpfeil <aaa>
2023-09-24 19:58:42 +0200

    time count

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,21 @@
 int main(int argc, char **argv) {
     try {
         auto loader = std::make_unique<Loader>();
+
         auto data_hr = loader->load_HR_data("../data/HR_007.csv");
         std::cout <<  "done hr " << data_hr.size() << std::endl;
+
+        auto start_time = std::chrono::high_resolution_clock::now();
+
         auto data_acc = loader->load_ACC_data("../data/ACC_007.csv");
         std::cout <<  "done acc " << data_acc.size() << std::endl;
+
+        auto end_time = std::chrono::high_resolution_clock::now();
+        // Calculate the duration
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+        // Print the duration in milliseconds
+        std::cout << "Time taken: " << duration.count() / 1000 << "seconds" << std::endl;
+
 
     }
     catch (std::exception & err) {
